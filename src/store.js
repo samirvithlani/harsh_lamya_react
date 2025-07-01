2987e4cc6d487d34e2a45ed480927a2bcd9c23ae
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-01 10:42:34 +0530

    add

--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux/CartSlice"
 import bankReducer from "./redux/BankSlice"
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore, PURGE } from "redux-persist";
+
+
+const rootReducer = combineReducers({
+    cart:cartReducer,
+    bank:bankReducer
+})
+
+const persistConfig ={
+    key:"root",
+    storage,
+    whitelist:['bank']
+
+}
 
 export const store = configureStore({
-    reducer:{
-        cart:cartReducer,
-        bank:bankReducer
+    reducer:persistReducer(persistConfig,rootReducer),
+    middleware:(getDefaultMiddleware)=>{
+        return getDefaultMiddleware({
+            serializableCheck:{
+                ignoreActions:[PURGE]
+            }
+        })
     }
+    
 })
+
+export const persistor = persistStore(store)
